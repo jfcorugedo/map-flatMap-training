f6d18b5aa9b18645328b963f7a80120cec158977
jfcorugedo <jfcorugedo@gmail.com>
2018-10-08 15:16:26 +0200

    improves some names

--- src/main/java/exercise/bookstore/business/impl/SummaryServiceImpl.java
+++ src/main/java/exercise/bookstore/business/impl/SummaryServiceImpl.java
@@ -20,24 +20,24 @@ public class SummaryServiceImpl implements SummaryService<GenericError> {
 	private final ServiceChapter<GenericError> srvChapter;
 	private final ServiceAuthor<GenericError> srvAuthor;
 	
-	private final MonadFutEither<GenericError> m;
+	private final MonadFutEither<GenericError> monadTransformer;
 	
 	
 	public SummaryServiceImpl(ServiceBook<GenericError> srvBook,
 			ServiceSales<GenericError> srvSales,
 			ServiceChapter<GenericError> srvChapter,
 			ServiceAuthor<GenericError> srvAuthor,
-			MonadFutEither<GenericError> m) {
+			MonadFutEither<GenericError> monadTransformer) {
 		super();
 		this.srvBook = srvBook;
 		this.srvSales = srvSales;
 		this.srvChapter = srvChapter;
 		this.srvAuthor = srvAuthor;
-		this.m = m;
+		this.monadTransformer = monadTransformer;
 	}
 
 	@Override
-	public Future<Either<GenericError, Summary>> getSummary(Integer idBook) {
+	public Future<Either<GenericError, Summary>> getSummary(Integer bookId) {
 
 
 		//TODO: Code the logic to build a Future<Either<GenericError, Summary>>
