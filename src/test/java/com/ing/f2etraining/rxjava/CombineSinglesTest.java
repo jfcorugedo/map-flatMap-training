af3acd498d8921830accc5b75ba568c4be3bf9c9
Juan Fernandez-Corugedo Igual <juan.fernandez-corugedo.igual@ing.com>
2021-05-20 17:11:13 +0200

    feat: adds first exercise with RxJava3 Single type (solved)

--- src/test/java/com/ing/f2etraining/rxjava/CombineSinglesTest.java
+++ src/test/java/com/ing/f2etraining/rxjava/CombineSinglesTest.java
@@ -1,28 +1,11 @@
 package com.ing.f2etraining.rxjava;
 
-import akka.dispatch.ExecutionContexts;
-import akka.dispatch.Futures;
-import akka.util.Timeout;
 import com.ing.f2etraining.model.Person;
-import errors.GenericError;
-import errors.impl.MyError;
 import io.reactivex.rxjava3.core.Single;
 import org.junit.Test;
-import scala.concurrent.Await;
-import scala.concurrent.ExecutionContext;
-import scala.concurrent.Future;
-import scala.concurrent.duration.Duration;
-import scala.util.Either;
-import scala.util.Left;
-import scala.util.Right;
-
-import java.util.concurrent.Executors;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CombineSinglesTest {
-
-    private static final Timeout TIMEOUT = new Timeout(Duration.create(5, "seconds"));
 
     @Test
     public void combineSeveralSinglesWithFlatMap() throws Exception {
@@ -31,8 +14,11 @@ public class CombineSinglesTest {
         Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
 
         //when
-        /* TODO */
-        Single<Integer> sumAges = null;
+        Single<Integer> sumAges = meSingle.flatMap(
+                me -> friendSingle.flatMap(
+                        friend -> Single.just(me.getAge() + friend.getAge())
+                )
+        );
 
         //then
         Integer result = sumAges.blockingGet();
@@ -46,11 +32,14 @@ public class CombineSinglesTest {
         Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
 
         //when
-        /* TODO */
-        Single<Integer> sumAges = null;
+        Single<Single<Integer>> sumAges = meSingle.map(
+                me -> friendSingle.map(
+                        friend -> me.getAge() + friend.getAge()
+                )
+        );
 
         //then
-        Integer result = sumAges.blockingGet();
+        Integer result = sumAges.blockingGet().blockingGet();
         assertThat(result).isEqualTo(63);
     }
 
@@ -61,8 +50,11 @@ public class CombineSinglesTest {
         Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
 
         //when
-        /* TODO */
-        Single<Integer> sumAges = null;
+        Single<Integer> sumAges = meSingle.flatMap(
+                me -> friendSingle.map(
+                        friend -> me.getAge() + friend.getAge()
+                )
+        );
 
         //then
         Integer result = sumAges.blockingGet();
@@ -75,10 +67,14 @@ public class CombineSinglesTest {
         Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
 
         //Hint: Use service Single<Person> getFriend(String name) to get the other person
+        Single<Person> friendSingle = meSingle.flatMap(me -> getFriend(me.getName()));
 
         //when
-        /* TODO */
-        Single<Integer> sumAges = null;
+        Single<Integer> sumAges = meSingle.flatMap(
+                me -> friendSingle.map(
+                        friend -> me.getAge() + friend.getAge()
+                )
+        );
 
         //then
         Integer result = sumAges.blockingGet();
@@ -88,27 +84,33 @@ public class CombineSinglesTest {
     @Test
     public void combineSeveralFuturesWithFailureResult() throws Exception {
         //given
-        Single<Person> meFuture = Single.error(new Exception("Unexpected error"));
+        Single<Person> meSingle = Single.error(new Exception("Unexpected error"));
         Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
 
         //when
-        /* TODO */
-        Future<Integer> sumAgeF = null;
+        Single<Integer> sumAges = meSingle.flatMap(
+                me -> friendSingle.map(
+                        friend -> me.getAge() + friend.getAge()
+                )
+        ).onErrorReturn(error -> -1);
 
         //then
-        Integer sumAge = (Integer) Await.result(sumAgeF, TIMEOUT.duration());
+        Integer sumAge = sumAges.blockingGet();
         assertThat(sumAge).isEqualTo(-1);
     }
 
     @Test
     public void combineSeveralFuturesWithAnotherFailureResult() throws Exception {
         //given
-        Future<Person> meFuture = Futures.successful(new Person().setName("Juan").setAge(35));
-        Future<Person> friendFuture = Futures.failed(new Exception("Another unexpected error"));
+        Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
+        Single<Person> friendSingle = Single.error(new Exception("Another unexpected error"));
 
         //when
-        /* TODO */
-        Single<Integer> sumAges = null;
+        Single<Integer> sumAges = meSingle.flatMap(
+                me -> friendSingle.map(
+                        friend -> me.getAge() + friend.getAge()
+                )
+        ).onErrorReturn(error -> -1);
 
         //then
         Integer result = sumAges.blockingGet();
