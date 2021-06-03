33104e6199ee84641315fe2d1dffdbda46497a46
Juan Fernandez-Corugedo Igual <juan.fernandez-corugedo.igual@ing.com>
2021-06-03 15:51:43 +0200

    feat: adds tests to use map2, map3 and map4 utils (solved)

--- src/test/java/com/ing/f2etraining/rxjava/CombineSinglesTest.java
+++ src/test/java/com/ing/f2etraining/rxjava/CombineSinglesTest.java
@@ -3,12 +3,14 @@ package com.ing.f2etraining.rxjava;
 import com.ing.f2etraining.model.Person;
 import io.reactivex.rxjava3.core.Single;
 import org.junit.Test;
+
+import static monad.rxjava.SingleUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CombineSinglesTest {
 
     @Test
-    public void combineSeveralSinglesWithFlatMap() throws Exception {
+    public void combineSeveralSinglesWithFlatMap() {
         //given
         Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
         Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
@@ -26,7 +28,7 @@ public class CombineSinglesTest {
     }
 
     @Test
-    public void combineSeveralFuturesWithMap() throws Exception {
+    public void combineSeveralFuturesWithMap() {
         //given
         Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
         Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
@@ -44,7 +46,7 @@ public class CombineSinglesTest {
     }
 
     @Test
-    public void combineSeveralFuturesWithFlatMapAndMap() throws Exception {
+    public void combineSeveralFuturesWithFlatMapAndMap() {
         //given
         Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
         Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
@@ -62,7 +64,63 @@ public class CombineSinglesTest {
     }
 
     @Test
-    public void combineSeveralDependentFuturesWithFlatMapAndMap() throws Exception {
+    public void combineTwoSingleWithSingleUtils() {
+        //given
+        Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
+        Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
+
+        //when
+        Single<Integer> sumAges = map2(meSingle, friendSingle, (me, friend) -> me.getAge() + friend.getAge());
+
+        //then
+        Integer result = sumAges.blockingGet();
+        assertThat(result).isEqualTo(63);
+    }
+
+    @Test
+    public void combineThreeSingleWithSingleUtils() {
+        //given
+        Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
+        Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
+        Single<Person> otherSingle = Single.just(new Person().setName("David").setAge(30));
+
+        //when
+        Single<Integer> sumAges = map3(
+                meSingle,
+                friendSingle,
+                otherSingle,
+                (me, frind, other) -> me.getAge() + frind.getAge() + other.getAge()
+        );
+
+        //then
+        Integer result = sumAges.blockingGet();
+        assertThat(result).isEqualTo(93);
+    }
+
+    @Test
+    public void combineFourSingleWithSingleUtils() {
+        //given
+        Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
+        Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
+        Single<Person> otherSingle = Single.just(new Person().setName("David").setAge(30));
+        Single<Person> anotherSingle = Single.just(new Person().setName("Nacho").setAge(21));
+
+        //when
+        Single<Integer> sumAges = map4(
+                meSingle,
+                friendSingle,
+                otherSingle,
+                anotherSingle,
+                (me, friend, other, another) -> me.getAge() + friend.getAge() + other.getAge() + another.getAge()
+        );
+
+        //then
+        Integer result = sumAges.blockingGet();
+        assertThat(result).isEqualTo(114);
+    }
+
+    @Test
+    public void combineSeveralDependentFuturesWithFlatMapAndMap() {
         //given
         Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
 
@@ -82,7 +140,7 @@ public class CombineSinglesTest {
     }
 
     @Test
-    public void combineSeveralFuturesWithFailureResult() throws Exception {
+    public void combineSeveralFuturesWithFailureResult() {
         //given
         Single<Person> meSingle = Single.error(new Exception("Unexpected error"));
         Single<Person> friendSingle = Single.just(new Person().setName("Luigi").setAge(28));
@@ -100,7 +158,7 @@ public class CombineSinglesTest {
     }
 
     @Test
-    public void combineSeveralFuturesWithAnotherFailureResult() throws Exception {
+    public void combineSeveralFuturesWithAnotherFailureResult() {
         //given
         Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
         Single<Person> friendSingle = Single.error(new Exception("Another unexpected error"));
@@ -118,7 +176,7 @@ public class CombineSinglesTest {
     }
 
     @Test
-    public void combineSeveralFuturesUsingFailureRecovery() throws Exception {
+    public void combineSeveralFuturesUsingFailureRecovery() {
         //given
         Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
         Single<Person> friendSingle = Single.error(new Exception("Another unexpected error"));
@@ -136,7 +194,7 @@ public class CombineSinglesTest {
     }
 
     @Test
-    public void combineSeveralFuturesUsingFallback() throws Exception {
+    public void combineSeveralFuturesUsingFallback() {
         //given
         Single<Person> meSingle = Single.just(new Person().setName("Juan").setAge(35));
         Single<Person> friendSingle = Single.error(new Exception("Another unexpected error"));
