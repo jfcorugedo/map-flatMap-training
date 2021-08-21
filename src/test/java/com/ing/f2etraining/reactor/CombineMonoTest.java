672f0bd0e09c36c3ab06d775a8cf711a263c651e
Juan Fernandez-Corugedo Igual <jfcorugedo@gmail.com>
2021-08-21 21:09:43 +0200

    feat: add Reactor tests suites

---
+++ src/test/java/com/ing/f2etraining/reactor/CombineMonoTest.java
@@ -0,0 +1,129 @@
+package com.ing.f2etraining.reactor;
+
+import com.ing.f2etraining.model.Person;
+import org.junit.Test;
+import reactor.core.publisher.Mono;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CombineMonoTest {
+
+    @Test
+    public void combineSeveralMonoWithFlatMap() {
+        //given
+        Mono<Person> meMono = Mono.just(new Person().setName("Juan").setAge(35));
+        Mono<Person> friendMono = Mono.just(new Person().setName("Luigi").setAge(28));
+
+        //when
+        /* TODO: You have to use flatMap */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(63);
+    }
+
+    @Test
+    public void combineSeveralMonoWithMap() {
+        //given
+        Mono<Person> meMono = Mono.just(new Person().setName("Juan").setAge(35));
+        Mono<Person> friendMono = Mono.just(new Person().setName("Luigi").setAge(28));
+
+        //when
+        /* TODO: You have to use Map */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(63);
+    }
+
+    @Test
+    public void combineSeveralMonoWithFlatMapAndMap() {
+        //given
+        Mono<Person> meMono = Mono.just(new Person().setName("Juan").setAge(35));
+        Mono<Person> friendMono = Mono.just(new Person().setName("Luigi").setAge(28));
+
+        //when
+        /* TODO: Use both flatMap and map */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(63);
+    }
+
+    @Test
+    public void combineTwoMonoWithMonoUtils() {
+        //given
+        Mono<Person> meMono = Mono.just(new Person().setName("Juan").setAge(35));
+        Mono<Person> friendMono = Mono.just(new Person().setName("Luigi").setAge(28));
+
+        //when
+        /* TODO: You can use util.reactor.MonoUtils to combine several Mono instances */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(63);
+    }
+
+    @Test
+    public void combineThreeMonoWithSingleUtils() {
+        //given
+        Mono<Person> meMono = Mono.just(new Person().setName("Juan").setAge(35));
+        Mono<Person> friendMono = Mono.just(new Person().setName("Luigi").setAge(28));
+        Mono<Person> otherMono = Mono.just(new Person().setName("David").setAge(30));
+
+        //when
+        /* TODO: Use class util.reactor.MonoUtils to combine several Mono objects */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(93);
+    }
+
+    @Test
+    public void combineFourSingleWithSingleUtils() {
+        //given
+        Mono<Person> meSingle = Mono.just(new Person().setName("Juan").setAge(35));
+        Mono<Person> friendMono = Mono.just(new Person().setName("Luigi").setAge(28));
+        Mono<Person> otherMono = Mono.just(new Person().setName("David").setAge(30));
+        Mono<Person> anotherMono = Mono.just(new Person().setName("Nacho").setAge(21));
+
+        //when
+        /* TODO: Use class util.reactor.MonoUtils to combine several Mono objects */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(114);
+    }
+
+    @Test
+    public void combineSeveralDependentFuturesWithFlatMapAndMap() {
+        //given
+        Mono<Person> meMono = Mono.just(new Person().setName("Juan").setAge(35));
+
+        //Hint: Use service getFriend(String name) to get the other person
+
+        //when
+        /* TODO */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(63);
+    }
+
+    /**
+     * This method simulates a call to another services that will return the friend of the given person
+     * @param name Name of the person whose friend we want to know
+     * @return Person object representing the friend of the input name
+     */
+    private Mono<Person> getFriend(String name) {
+
+        return Mono.just(new Person().setName("Luigi").setAge(28));
+    }
+}
---
+++ src/test/java/com/ing/f2etraining/reactor/ErrorHandlingWithMonoTest.java
@@ -0,0 +1,89 @@
+package com.ing.f2etraining.reactor;
+
+import com.ing.f2etraining.model.Person;
+import org.junit.Test;
+import reactor.core.publisher.Mono;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Practice with some methods inside RxJava to deal with errors:
+ * - onErrorReturn
+ * - onErrorResumeWith
+ * - onErrorResumeNext
+ */
+public class ErrorHandlingWithMonoTest {
+
+    @Test
+    public void combineSeveralMonoWithFailureResult() {
+        //given
+        Mono<Person> meMono = Mono.error(new Exception("Unexpected error"));
+        Mono<Person> friendMono = Mono.just(new Person().setName("Luigi").setAge(28));
+
+        //when
+        /* TODO */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    public void combineSeveralMonoWithAnotherFailureResult() {
+        //given
+        Mono<Person> meMono = Mono.just(new Person().setName("Juan").setAge(35));
+        Mono<Person> friendMono = Mono.error(new Exception("Another unexpected error"));
+
+        //when
+        /* TODO */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    public void combineSeveralMonoUsingFailureRecovery() {
+        //given
+        Mono<Person> meMono = Mono.just(new Person().setName("Juan").setAge(35));
+        Mono<Person> friendMono = Mono.error(new Exception("Another unexpected error"));
+
+        //when
+        /* TODO: Use failureRecovery function */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(-2);
+    }
+
+    @Test
+    public void combineSeveralMonoUsingFallback() {
+        //given
+        Mono<Person> meMono = Mono.just(new Person().setName("Juan").setAge(35));
+        Mono<Person> friendMono = Mono.error(new Exception("Another unexpected error"));
+
+        //when
+        /* TODO: Use fallback function */
+        Mono<Integer> sumAges = null;
+
+        //then
+        Integer result = sumAges.block();
+        assertThat(result).isEqualTo(-3);
+    }
+
+    private Mono<Person> getFriend(String name) {
+
+        return Mono.just(new Person().setName("Luigi").setAge(28));
+    }
+
+    private Mono<Integer> failureRecovery(Throwable error) {
+        return Mono.just(-2);
+    }
+
+    private Mono<Integer> fallback() {
+        return Mono.just(-3);
+    }
+}
